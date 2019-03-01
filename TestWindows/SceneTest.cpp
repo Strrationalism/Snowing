7732fecaecdbbeca3b90ace1e7d4928b67cabecf
星翼 <853974536@qq.com>
2019-03-01 15:08:07 +0800

    Bug fixed

--- TestWindows/SceneTest.cpp
+++ TestWindows/SceneTest.cpp
@@ -192,7 +192,7 @@ TEST(SceneTest, TestTextMenuItem)
 		Snowing::Graphics::EffectTech tech2 = eff.LoadTechnique("FontTestBasic", Sprite::DataLayout);
 		Snowing::Graphics::Buffer gpuVBuffer = FontRenderer<>::MakeGPUVertexBuffer();
 
-		Font font = LoadFont(LoadAsset(L"Font-chs.fnt"));
+		Font font = LoadFont(LoadAsset("Font-chs.fnt"));
 		FontRenderer<> fontRenderer
 		{
 			&Graphics::Device::Get().MainContext(),
@@ -266,7 +266,7 @@ TEST(SceneTest, TestMenuKeyController)
 		Snowing::Graphics::EffectTech tech2 = eff.LoadTechnique("FontTestBasic", Sprite::DataLayout);
 		Snowing::Graphics::Buffer gpuVBuffer = FontRenderer<>::MakeGPUVertexBuffer();
 
-		Font font = LoadFont(LoadAsset(L"Font-chs.fnt"));
+		Font font = LoadFont(LoadAsset("Font-chs.fnt"));
 		FontRenderer<> fontRenderer
 		{
 			&Graphics::Device::Get().MainContext(),
@@ -342,7 +342,7 @@ TEST(SceneTest, TestMenuPositionController)
 		Snowing::Graphics::EffectTech tech2 = eff.LoadTechnique("FontTestBasic", Sprite::DataLayout);
 		Snowing::Graphics::Buffer gpuVBuffer = FontRenderer<>::MakeGPUVertexBuffer();
 
-		Font font = LoadFont(LoadAsset(L"Font-chs.fnt"));
+		Font font = LoadFont(LoadAsset("Font-chs.fnt"));
 		FontRenderer<> fontRenderer
 		{
 			&Graphics::Device::Get().MainContext(),
@@ -403,7 +403,7 @@ static void TestDebugDisplay(Math::Vec2<int> size)
 		L"FPSDisplay",
 		size,
 		true);
-	Font font = LoadFont(LoadAsset(L"Font-chs.fnt"));
+	Font font = LoadFont(LoadAsset("Font-chs.fnt"));
 	Graphics::Effect eff{ LoadAsset("HiLevelRendering") };
 	Graphics::EffectTech tech1 = eff.LoadTechnique("FontTestBasic", Sprite::DataLayout);
 	Scene::Group<> g;
@@ -434,7 +434,7 @@ static void TestDebugMenu()
 		L"TestDebugMenu",
 		{ 800,600 },
 		true);
-	Font font = LoadFont(LoadAsset(L"Font-chs.fnt"));
+	Font font = LoadFont(LoadAsset("Font-chs.fnt"));
 	Graphics::Effect eff{ LoadAsset("HiLevelRendering") };
 	Graphics::EffectTech tech1 = eff.LoadTechnique("FontTestLighting", Sprite::DataLayout);
 	Scene::Group<> g;
