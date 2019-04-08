590a367147c8a78a4ddf4f4080f7733efd51e7c1
星翼 <853974536@qq.com>
2019-04-08 19:36:12 +0800

    Vec3

--- Core/Vec3.h
+++ Core/Vec3.h
@@ -11,6 +11,38 @@ namespace Snowing::Math
 	using Vec3f = Vec3<float>;
 
 	template <typename A, typename B>
+	constexpr Vec3<A> operator + (const Vec3<A>& a, const Vec3<B>& b)
+	{
+		return {
+			a.x + b.x,
+			a.y + b.y,
+			a.z + b.z
+		};
+	}
+
+	template <typename A, typename B>
+	constexpr Vec3<A> operator - (const Vec3<A>& a, const Vec3<B>& b)
+	{
+		return {
+			a.x - b.x,
+			a.y - b.y,
+			a.z - b.z
+		};
+	}
+
+	template <typename A, typename B>
+	[[nodiscard]]
+	constexpr Vec3<A> operator * (const Vec3<A>& a, const B& b) noexcept
+	{
+		return
+		{
+			a.x * b,
+			a.y * b,
+			a.z * b
+		};
+	}
+
+	template <typename A, typename B>
 	[[nodiscard]]
 	constexpr bool operator == (const Vec3<A>& a, const Vec3<B>& b)
 	{
