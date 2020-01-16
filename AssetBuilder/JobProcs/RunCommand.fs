a7659d1107a53dfdd4796c8fe1f055c0b7fc204c
Seng Jik <853974536@qq.com>
2020-01-16 13:32:48 +0800

    bug fixed

---
+++ AssetBuilder/JobProcs/RunCommand.fs
@@ -0,0 +1,29 @@
+﻿module JobProcs.RunCommand
+
+open Job
+
+let private runCommandProc job =
+    Utils.StartWait (job.ScriptDir.FullName + "\\" + job.Arguments.[0]) 
+        (job.Input
+        |> List.reduce (fun a b -> a + " " + b))
+
+let FinalRunProc = {
+    Proc = runCommandProc
+    InputType = InputType.None
+    Command = "FinalRun"
+    FinishLogEnabled = true
+    Prority = System.Int32.MaxValue
+}
+
+let PostRunProc = {
+    FinalRunProc with
+        Command = "PostRun"
+        Prority = System.Int32.MaxValue - 1
+}
+
+let PreRunProc = {
+    FinalRunProc with
+        Command = "PreRun"
+        Prority = 0
+}
+
